d7d2f5f0bdc58c4f8101ffa78bba5402f76bf6fe
Pathan-Faizann <faizanpathan8104@gmail.com>
2025-09-08 17:37:26 +0530

    responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <>
     <div className='body d-flex justify-content-center'>
-      <div className='d-flex f-container'>
+      <div className='d-flex  main-container'>
         <div className=' first p-4'>
-          <div className='d-flex flex-column h-100 justify-content-between'>
+          <div className='d-flex flex-column h-100 justify-content-between align-items-center'>
             <h2 className='m-4 fs-1 logo'>Better <br /> Market</h2>
-            <h2 className='tagline text-start ms-4'>Say hello to global food and beverage producers and suppliers, all in one place</h2>
+            <span className='tagline text-start ms-4'>Say hello to global food and beverage producers and suppliers, all in one place</span>
           </div>
 
         </div> 
@@ -29,9 +29,9 @@ function App() {
               <div><label htmlFor="" className='form-label fw-bold mt-4'>Password</label>
               <br />
               <input type="password" className='' /></div>
-              <button onClick={(e)=> e.preventDefault()} className='mt-5 p-4'>Sign up</button>
+              <button onClick={(e)=> e.preventDefault()} className='mt-5 p-4 f-btn'>Sign up</button>
             </form>
-            <p className='mt-5'>By signing up, you agree to our <u>Terms of Service.</u> <br />
+            <p className='mt-5 '>By signing up, you agree to our <u>Terms of Service.</u> <br />
 Already have an account? <a className='underline'> Log in </a></p>
           </div>
 
